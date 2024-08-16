4a85b01531e9425f737b41ae90d14570af34ea41
DioneKx <mjhonnyvieira@gmail.com>
2024-08-16 20:09:52 -0300

    Jhonny: Control-Tabs

---
+++ Cntrol Tabs/control_tabs/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace control_tabs
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		TabControl tabs = new TabControl();
+		Button btn = new Button();
+		
+		string[] tab_title = new string[]
+		{
+			"Average", "Strength", "Name Formatting", "Date formatting", "Income"
+		};
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			this.MaximizeBox = false;
+			this.FormBorderStyle = FormBorderStyle.FixedSingle;
+			
+			tabs.Width = this.Width - 50;
+			tabs.Height = this.Height - 70;
+			tabs.Left = 17; tabs.Top = 15;
+			tabs.Parent = this;
+			
+			for (int i = 0; i < 5; i++)
+			{
+				TabPage tab = new TabPage();
+				tab.Name = "tab" + i.ToString();
+				tab.Text = tab_title[i];
+				tab.BackColor = Color.White;
+				tabs.TabPages.Add(tab);
+			}
+		}
+		
+		object SearchObject(string name)
+		{
+			foreach (Control control in this.Controls)
+			{
+				if (control is TabPage || control is TextBox || control is Button)
+				{
+					if (control.Name.Contains(name))
+					{
+						return control;
+					}
+				}
+			}
+			
+			return null;
+		}
+	}
+}
