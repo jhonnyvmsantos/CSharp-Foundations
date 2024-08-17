5fa095a7ef42f368567af291ca738c267b38c0d1
DioneKx <mjhonnyvieira@gmail.com>
2024-08-17 18:44:57 -0300

    Jhonny: Tab1-Front-Complete

--- Cntrol Tabs/control_tabs/MainForm.cs
+++ Cntrol Tabs/control_tabs/MainForm.cs
@@ -51,53 +51,73 @@ namespace control_tabs
 //			INNER CONTENT (TAB_PAGE 0)
 			for (int i = 0; i < 6; i++)
 			{
-				TabPage page = tabs.TabPages[0] as TabPage;
-
 				if (i < 5)
 				{
-					Label label = new Label();
-					label.AutoSize = true;
-					label.Name = "page0_label" + i.ToString();
-					label.Text = text_inner_pages[0][0] + i.ToString();
-					label.Location = new Point(60, 65 * (i + 1) - 20);
-					label.Font = new Font(FontFamily.GenericSerif, 12f);
-					label.Parent = page;
-					
-					TextBox entry = new TextBox();
-					entry.Name = "page0_entry" + i.ToString();
-					entry.Location = new Point(60, 65 * (i + 1) + 5);
-					entry.Width = 120;
-					entry.Parent = page;
+					CreateFormEntry(
+						useful: new int[] {0, i},
+						text_label: text_inner_pages[0][0] + i.ToString(),
+						element_location: new int[] {
+							60, 65 * (i + 1) - 20, 
+							60,  65 * (i + 1) + 5
+						}
+					);
 				}
 				else
 				{
-					Button calc = new Button();
-					calc.Name = "page0_button" + i.ToString();
-					calc.Tag = "average";
-					calc.Text = text_inner_pages[0][2];
-					calc.Location = new Point(page.Width - 160, (page.Height / 2) - 50);
-					calc.Size = new Size(100, 30);
-					calc.Click += ButtonClick;
-					calc.Parent = page;
-					
-					Label label = new Label();
-					label.AutoSize = true;
-					label.Name = "page0_label" + i.ToString();
-					label.Text = text_inner_pages[0][1];
-					label.Location = new Point(page.Width - 140, page.Height / 2);
-					label.Font = new Font(FontFamily.GenericSerif, 12f);
-					label.Parent = page;
-					
-					TextBox entry = new TextBox();
-					entry.Name = "page0_entry" + i.ToString();
-					entry.Location = new Point(page.Width - 160, (page.Height / 2) + 25);
-					entry.Width = 100; entry.ReadOnly = true;
-					entry.Parent = page;
+					CreateFormOutput(
+						useful: new int[] {0, i},
+						element_text: new string[] {
+							text_inner_pages[0][2],
+							text_inner_pages[0][1]
+						},
+						button_tag: "average",
+						element_location: new int[] {
+							tabs.Width - 180, (tabs.Height / 2) - 50,
+							tabs.Width - 160, tabs.Height / 2,
+							tabs.Width - 180, (tabs.Height / 2) + 25
+						}
+					);
 				}
 			}
 			
 //			INNER CONTENT (TAB_PAGE 1)
+			for (int i = 0; i < 3; i++)
+			{
+				TabPage page = tabs.TabPages[1] as TabPage;
+
+				if (i < 2)
+				{
+					CreateFormEntry(
+						useful: new int[] {1, i},
+						text_label: text_inner_pages[1][i],
+						element_location: new int[] {
+							60, 65 * (i + 1) + 65,
+							60, 65 * (i + 1) + 90
+						}
+					);
+				}
+				else{
+					CreateFormOutput(
+						useful: new int[] {1, i},
+						element_text: new string[] {
+							text_inner_pages[1][3],
+							text_inner_pages[1][2]
+						},
+						button_tag: "strength",
+						element_location: new int[] {
+							tabs.Width - 180, (tabs.Height / 2) - 50,
+							tabs.Width - 160, tabs.Height / 2,
+							tabs.Width - 180, (tabs.Height / 2) + 25
+						}
+					);
+				}
+			}
+			
 //			INNER CONTENT (TAB_PAGE 2)
+			for (int i = 0; i < 4; i++)
+			{
+				
+			}
 //			INNER CONTENT (TAB_PAGE 3)
 //			INNER CONTENT (TAB_PAGE 4)
 		}
@@ -140,6 +160,7 @@ namespace control_tabs
 						catch
 						{
 							MessageBox.Show("Por favor, utilize números inteiros - flutuantes, apenas...\nUtilize \",\" para números decimais");
+							total = 0;
 							break;
 						}
 					}
@@ -147,10 +168,60 @@ namespace control_tabs
 					if (entry == null)
 					{
 						entry = SearchTabObject("page0_entry5", 0) as TextBox;
-						entry.Text = (total / 5).ToString();
+						entry.Text = (total != 0) ? (total / 5).ToString() : "";
 					}
 					break;
+					
+				case "strength":
+					break;
 			}
+		}
+		
+		void CreateFormEntry(int[] useful, string text_label, int[]element_location)
+		{
+			TabPage tab = tabs.TabPages[useful[0]] as TabPage;
+			
+			Label label = new Label();
+			label.AutoSize = true;
+			label.Name = "page" + useful[0].ToString() + "_label" + useful[1].ToString();
+			label.Text = text_label;
+			label.Location = new Point(element_location[0], element_location[1]);
+			label.Font = new Font(FontFamily.GenericSerif, 12f);
+			label.Parent = tab;
+			
+			TextBox entry = new TextBox();
+			entry.Name = "page" + useful[0].ToString() + "_entry" + useful[1].ToString();
+			entry.Location = new Point(element_location[2], element_location[3]);
+			entry.Width = 120;
+			entry.Parent = tab;
+		}
+		
+		void CreateFormOutput(int[] useful, string[] element_text, string button_tag, int[] element_location)
+		{
+			TabPage tab = tabs.TabPages[useful[0]] as TabPage;
+			
+			Button calc = new Button();
+			calc.Name = "page" + useful[0] + "_button" + useful[1].ToString();
+			calc.Tag = button_tag;
+			calc.Text = element_text[0];
+			calc.Location = new Point(element_location[0], element_location[1]);
+			calc.Size = new Size(100, 30);
+			calc.Click += ButtonClick;
+			calc.Parent = tab;
+			
+			Label label = new Label();
+			label.AutoSize = true;
+			label.Name = "page" + useful[0] + "_label" + useful[1].ToString();
+			label.Text = element_text[1];
+			label.Location = new Point(element_location[2], element_location[3]);
+			label.Font = new Font(FontFamily.GenericSerif, 12f);
+			label.Parent = tab;
+			
+			TextBox entry = new TextBox();
+			entry.Name = "page" + useful[0] + "_entry" + useful[1].ToString();
+			entry.Location = new Point(element_location[4], element_location[5]);
+			entry.Width = 100; entry.ReadOnly = true;
+			entry.Parent = tab;
 		}
 	}
 }
